b1f0355532bb9abc4372954e5b7f1bf1c647b16b
Gustavo <gmonte@tutanota.com>
2022-05-05 04:04:25 -0700

    custom SVG and positioning

--- src/App.js
+++ src/App.js
@@ -79,8 +79,15 @@ const App = () => {
             <Marker
               key={marker.time}
               position={{
+
                 lat: marker.lat,
                 lng: marker.lng
+              }}
+              icon={{
+                url: "/avatar-boy-male-5-svgrepo-com.svg",
+                scaledSize: new window.google.maps.Size(30, 30),
+                origin: new window.google.maps.Point(0, 0),
+                anchor: new window.google.maps.Point(15, 15)
               }}
             />
           )
